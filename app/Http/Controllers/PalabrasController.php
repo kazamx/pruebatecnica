c27cf7fcab7bda9639f8caf9182cb2c936e97f34
kazamx <kazam84@gmail.com>
2020-09-14 08:47:53 -0500

    Funcionando V1.0

--- app/Http/Controllers/PalabrasController.php
+++ app/Http/Controllers/PalabrasController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use PHPUnit\Util\Json;
-use stdClass;
 
 // use App\Http\Controllers\DB;
 
@@ -18,45 +16,56 @@ class PalabrasController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request){
-    $tiempo_inicio = microtime(true);
-    $datos = DB::select('select sin_acentos from palabras where CHARACTER_LENGTH(sin_acentos) = ?', [$request->cantidad]);
-    $salida[] = "";
-    $array = str_split($request->palabra, 1); // 1. Es mejor que haga esto una sola vez.
+    public function __invoke(Request $request)
+    {
+        $cantidad = $request->cantidad;
+        $palabra = ltrim($request->palabra);
 
-    foreach ($datos as $dato) {
-        
-        $dato_array = str_split($dato->sin_acentos, 1);
-        $contador_igualdades = 0;
 
-        for ($i = 0; $i < count($array); $i++) {
-            for ($j = 0; $j < count($dato_array); $j++) { // (1) Se deben invertir las palabras para que no
-                                                     // sea obligatorio realizar el punto 1 varias veces.
-                if ($array[$i] === $dato_array[$j]) {
-                    $contador_igualdades++;
-                    $dato_array[$j] = null;
-                    $j = count($dato_array);
+        if($cantidad >2 && $cantidad != null && $palabra != null && strlen($palabra) > $cantidad){
+            
+
+            $tiempo_inicio = microtime(true);
+        $datos = DB::select('select sin_acentos from palabras where CHARACTER_LENGTH(sin_acentos) = ?', [$cantidad]);
+        $salida[] = "";
+        $array = str_split($palabra, 1); // 1. Es mejor que haga esto una sola vez.
+
+        foreach ($datos as $dato) {
+
+            $dato_array = str_split($dato->sin_acentos, 1);
+            $contador_igualdades = 0;
+
+            for ($i = 0; $i < count($array); $i++) {
+                for ($j = 0; $j < count($dato_array); $j++) { // (1) Se deben invertir las palabras para que no
+                    // sea obligatorio realizar el punto 1 varias veces.
+                    if ($array[$i] === $dato_array[$j]) {
+                        $contador_igualdades++;
+                        $dato_array[$j] = null;
+                        $j = count($dato_array);
+                    }
                 }
+            }
+
+            if ($contador_igualdades === count($dato_array)) {
+                array_push($salida, $dato->sin_acentos);
             }
         }
 
-        if ($contador_igualdades === count($dato_array)) {
-            array_push($salida, $dato->sin_acentos);
-        }
+        unset($salida[0]);
+
+        $tiempo_fin = microtime(true);
+        // $salida_json = new stdClass();
+        // $salida_json->tiempo = $tiempo_fin - $tiempo_inicio;
+        $tiempo = $tiempo_fin - $tiempo_inicio;
+        $cantidad = count($salida);
+        // $salida_json->palabras = $salida;
+        // $salida_json = json_encode($salida_json);
+
+        return view('palabras', compact('salida', 'tiempo', 'cantidad',));
+        // return ($salida);
+
+        } else{
+            return "ERROR: Digite un dato válido";
+        }       
     }
-
-    unset($salida[0]);
-
-    $tiempo_fin = microtime(true);
-    // $salida_json = new stdClass();
-    // $salida_json->tiempo = $tiempo_fin - $tiempo_inicio;
-    $tiempo = $tiempo_fin - $tiempo_inicio;
-    $cantidad = count($salida);
-    // $salida_json->palabras = $salida;
-    // $salida_json = json_encode($salida_json);
-
-    return view('palabras',compact('salida','tiempo','cantidad'));
-    // return ($salida);
-}
-
 }
--- resources/views/palabras.blade.php
+++ resources/views/palabras.blade.php
@@ -1,64 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+  <meta charset="utf-8">
+  <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="{{url('plugins/fontawesome-free/css/all.min.css')}}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{url('dist/css/adminlte.min.css')}}">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+    <title>Palabras</title>
 </head>
 <body>
     <h1>Busqueda finalizada</h1>
     <h3>Coinciden {{ $cantidad }} palabras con tu busqueda</h3>
     <h3>El programa tardó {{ $tiempo }} segundos en ejecutarse</h3>
     
-    {{-- <ol>
-        @foreach ($salida as $data)
-            <li>{{ $data }}</li>
-        @endforeach
-    </ol> --}}
 
-            {{ $i=1 }}
-            @while($salida != null)
-                <tr>
-                    <td> {{ $salida[$i] }} </td>
-                    <td> {{ $salida[$i+1] }} </td>
-                    <td> {{ $salida[$i+2] }} </td>
-                    <td> {{ $salida[$i+3] }} </td>
-                    <td> {{ ($salida[$i+4] != null) ? $salida[$i+4] : null }} </td>
-                    
-                </tr>
-                {{ $i++ }}
-            @endwhile
-
-
-    {{-- <table>
-        {{ $cont = 0 }}
-        @foreach ($salida as $data)
-
-            
-
-            @if($cont%10 == 0)
-            <td> {{ $data }} </td>
-            @endif
-            <tr>
-                
-            </tr>
-        @endforeach
+    <a href={{ route('formulario') }} class="btn btn-success d-inline-flex btn-sm">Inicio</a>
+    <br/>
+    <br/>
+    <ul class="list-group">
         
-</table> --}}
-
-
-    {{-- <table>
-        
-            @for($i = 0; $i < 10; $i++)
-                <tr>
-                    @foreach ($salida as $data)
-                    <td> {{ $data }} </td>
-                    @endforeach
-                </tr>
-            @endfor
-            
-    </table> --}}
+        @foreach ($salida as $data)
+            <li class=list-group-item>{{ $data }}</li>
+        @endforeach
+    </ul>
 
 </body>
 </html>
